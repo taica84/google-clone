5938a2ae47309c419377a5c028b7e4e66dd2d98f
taica84 <taica_rules@yahoo.com>
2021-06-19 17:54:32 +0300

    disable eslint

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images : {
     domains : ['www.ptm.ro']
-  }
+  },
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 }
